e3abaea8ab1f537f877d7ed5838dcef49a59d398
kelin.zrh <kelin.zrh@antgroup.com>
2023-12-17 15:15:08 +0800

    feat: 项目初始化启动成功

--- src/main/java/com/kelin/seckill/SeckillApplication.java
+++ src/main/java/com/kelin/seckill/SeckillApplication.java
@@ -1,9 +1,11 @@
 package com.kelin.seckill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.kelin.seckill.pojo")
 public class SeckillApplication {
 
 	public static void main(String[] args) {
